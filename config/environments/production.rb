bf8db20106dfaf3fe34e89961ac4b10014dffe68
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2023-06-27 18:30:25 -0300

    Allow railway production domain as a host
    Add host to production and also development environment because railway
    seems to use development. Otherwise the middleware Action Dispatch
    blocks all access.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Allow railway production domain as a host.
+  config.hosts << "rafa-website-production.up.railway.app"
 end
